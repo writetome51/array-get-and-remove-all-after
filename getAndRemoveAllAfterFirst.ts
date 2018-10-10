f16bbc13d2b7ce5b1656ef1a21cfe2ab46497148
Steve Thompson <writetome51@gmail.com>
2018-10-10 00:58:14 -0600

    added tests

--- getAndRemoveAllAfterFirst.ts
+++ getAndRemoveAllAfterFirst.ts
@@ -1,9 +1,11 @@
 import { _getAndRemoveAllBefore_or_AfterFirst }
 	from '@writetome51/array-get-and-remove-all-before-or-after/_getAndRemoveAllBefore_or_AfterFirst';
+import { errorIfNotPrimitiveOrArray } from 'error-if-not-primitive-or-array';
 
 
 // value cannot be object
 
 export function getAndRemoveAllAfterFirst(value: any, array): any[] {
+	errorIfNotPrimitiveOrArray(value);
 	return _getAndRemoveAllBefore_or_AfterFirst(value, 'after', array);
 }
--- getAndRemoveAllAfterLast.js
+++ getAndRemoveAllAfterLast.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var _getAndRemoveAllBefore_or_AfterLast_1 = require("@writetome51/array-get-and-remove-all-before-or-after/_getAndRemoveAllBefore_or_AfterLast");
+var error_if_not_primitive_or_array_1 = require("error-if-not-primitive-or-array");
 // value cannot be object.
 function getAndRemoveAllAfterLast(value, array) {
+    error_if_not_primitive_or_array_1.errorIfNotPrimitiveOrArray(value);
     return _getAndRemoveAllBefore_or_AfterLast_1._getAndRemoveAllBefore_or_AfterLast(value, 'after', array);
 }
 exports.getAndRemoveAllAfterLast = getAndRemoveAllAfterLast;
