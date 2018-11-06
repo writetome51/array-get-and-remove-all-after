2643baf5c9e4b041f7af96d43a56d2c2bb11bb8b
Steve Thompson <writetome51@gmail.com>
2018-11-05 23:11:12 -0700

    added index.js file

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports.getAndRemoveAllAfterFirst = require('./lib/getAndRemoveAllAfterFirst').getAndRemoveAllAfterFirst;
+exports.getAndRemoveAllAfterLast = require('./lib/getAndRemoveAllAfterLast').getAndRemoveAllAfterLast;
